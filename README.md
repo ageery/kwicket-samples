0a72fa470534f3fbf7095674eea5ee944c37b2c1
ageery <andrew_geery@windsorsolutions.com>
2018-03-16 23:24:11 -0400

    Clean up and documentation.

---
+++ README.md
@@ -0,0 +1,15 @@
+kWicket Samples
+===============
+
+Overview
+--------
+
+Sample Kotlin Wicket applications using Spring Boot.
+
+Details
+-------
+
+* The Gradle files are written in Kotlin (*.gradle.kts)
+* The individual sample applications depend on the `sample` module. This module
+takes care of including most of the dependencies that the individual sample
+applications need and defines the base page that the individual applications extend.
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,30 +1,15 @@
 import org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val x: String by extra { "hello" }
-
 plugins {
     val kotlinVersion = "1.2.30"
-    //id("org.jetbrains.kotlin.jvm") version kotlinVersion
-    //val kotlinVersion = "1.2.30"
     val bootVersion = "2.0.0.RELEASE"
     val dependencyManagementVersion = "1.0.4.RELEASE"
     id("org.springframework.boot") version bootVersion
     id("org.jetbrains.kotlin.jvm") version kotlinVersion
     id("org.jetbrains.kotlin.plugin.spring") version kotlinVersion
     id("io.spring.dependency-management") version dependencyManagementVersion
-
 }
-
-// version = "1.0.0-SNAPSHOT"
-//
-//java.sourceSets["main"].resources {
-//    srcDir("src/main/kotlin")
-//}
-
-//kotlin {
-//    experimental.coroutines = ENABLE
-//}
 
 repositories {
     mavenCentral()
@@ -33,10 +18,8 @@ repositories {
 }
 
 allprojects {
-
     group = "org.kwicket.samples"
     version = "1.0.0-SNAPSHOT"
-
 }
 
 subprojects {
